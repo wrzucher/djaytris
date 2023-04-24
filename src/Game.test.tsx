113aa9ada46724ca852fa5c9535b8d83195c9cc7
SonyaSemchyshyn <96415772+SonyaSemchyshyn@users.noreply.github.com>
2023-04-24 21:13:44 +0300

    Add -we can reverse object- test

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -63,3 +63,28 @@ test('Object should freeze after stuck and start from top', () => {
     [0,0,2,2,0],
   ]);
 });
+test('We can reverse object', () => {
+  // Arrange
+  const gameField = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+  ];
+  const player_y = 3; // Player almost on the ground
+  const game = new Game(gameField, GameBlockType.Line, 0, player_y);
+  game.Initialize();
+
+  // Act
+  game.RotateLeft();
+
+  // Assert
+  expect(game.GameField).toStrictEqual([
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+  ]);
+});
